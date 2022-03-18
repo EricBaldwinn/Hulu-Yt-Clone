3d4de667feaa9e4635399b2bbf8bd22acbf9a364
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-03-18 15:47:51 -0700

    Setup tailwind / nextjs

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <h1>Lets build hulu 2.0</h1>
+
+    {/* Header component */}
+
+
+    {/* Nav */}
+    </div>
+  )
+}
