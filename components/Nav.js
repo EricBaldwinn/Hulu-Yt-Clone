a3c6e8363197104d17e767c85dc1662ad848a1a4
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-03-21 12:56:26 -0700

    Created requests/categories for nav bar

---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import requests from "../utils/requests.js";
+
+
+
+export const Nav = () => {
+
+
+  return (
+    <nav>
+      <div>
+        {Object.entries(requests).map(([key, {title, url}]) => (
+          <h2>{title}</h2>
+        ))}
+      </div>
+    </nav>
+  )
+}
